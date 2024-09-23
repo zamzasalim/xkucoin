23d9be34ce1129bdd408a997beb07fe8f3ca0674
zamzasalim <salimzamza@gmail.com>
2024-09-23 20:06:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,198 @@
+import time
+import requests
+import random
+import urllib.parse
+import os
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def art():
+    print("\033[1;91m" + r"""     
+
+================================================
+                AIRDROP ASC                        
+================================================
+     Bot : XKUCOIN
+     Telegram Channel : @airdropasc
+     Telegram Group : @autosultan_group
+================================================        
+""" + "\033[0m" + "\033[1;92m" + r"""                                    
+""" + "\033[0m\n\033[1;96m\033[0m")           
+
+def countdown_timer(seconds):
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        hours, mins = divmod(mins, 60)
+        print(f"{Fore.CYAN + Style.BRIGHT}Wait {hours:02}:{mins:02}:{secs:02}", end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("Wait 00:00:00          ", end='\r')
+
+def read_data_file(file_path):
+    accounts = []
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            encoded_data = line.strip()
+            if encoded_data:
+                accounts.append(encoded_data)
+    return accounts
+
+def decode_data(encoded_data):
+    params = dict(item.split('=') for item in encoded_data.split('&'))
+
+    decoded_user = urllib.parse.unquote(params['user'])
+    decoded_start_param = urllib.parse.unquote(params['start_param'])
+
+    return {
+        "decoded_user": decoded_user,
+        "decoded_start_param": decoded_start_param,
+        "hash": params['hash'],
+        "auth_date": params['auth_date'],
+        "chat_type": params['chat_type'],
+        "chat_instance": params['chat_instance']
+    }
+
+def login(decoded_data):
+    url = "https://www.kucoin.com/_api/xkucoin/platform-telebot/game/login?lang=en_US"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Chromium\";v=\"111\", \"Not(A:Brand\";v=\"8\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-request-with": "null",
+        "Referer": "https://www.kucoin.com/miniapp/tap-game?inviterUserId=5496274031&rcode=QBSTAPN3"
+    }
+    
+    body = {
+        "inviterUserId": "5496274031",
+        "extInfo": {
+            "hash": decoded_data['hash'],
+            "auth_date": decoded_data['auth_date'],
+            "via": "miniApp",
+            "user": decoded_data['decoded_user'],
+            "chat_type": decoded_data['chat_type'],
+            "chat_instance": decoded_data['chat_instance'],
+            "start_param": decoded_data['decoded_start_param']
+        }
+    }
+
+    session = requests.Session()
+    response = session.post(url, headers=headers, json=body)
+    cookie = '; '.join([f"{cookie.name}={cookie.value}" for cookie in session.cookies])             
+    return cookie
+
+def data(cookie):
+    url = "https://www.kucoin.com/_api/xkucoin/platform-telebot/game/summary?lang=en_US"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Chromium\";v=\"111\", \"Not(A:Brand\";v=\"8\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-request-with": "null",
+        "Referer": "https://www.kucoin.com/miniapp/tap-game?inviterUserId=5496274031&rcode=QBSTAPN3",
+        "cookie": cookie
+    }
+    
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    balance = data.get("data").get("availableAmount")
+    molecule = data.get("data").get("feedPreview").get("molecule")
+    print(f"{Fore.GREEN + Style.BRIGHT}Balance: {Fore.WHITE + Style.BRIGHT}{balance}")
+    return molecule
+
+def tap(cookie, molecule):
+    url = "https://www.kucoin.com/_api/xkucoin/platform-telebot/game/gold/increase?lang=en_US"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded",
+        "sec-ch-ua": "\"Chromium\";v=\"111\", \"Not(A:Brand\";v=\"8\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-request-with": "null",
+        "Referer": "https://www.kucoin.com/miniapp/tap-game?inviterUserId=5496274031&rcode=QBSTAPN3",
+        "cookie": cookie
+    }
+
+    total_increment = 0
+
+    while total_increment < 3000:
+        increment = random.randint(55, 60)  # Randomize increment value each iteration
+        form_data = {
+            'increment': str(increment),
+            'molecule': str(molecule)
+        }
+
+        response = requests.post(url, headers=headers, data=form_data)
+        total_increment += increment
+        
+        colors = [Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.RED]
+        random_color = random.choice(colors)
+        print(f"{random_color}{Style.BRIGHT}Tapped: {Fore.WHITE + Style.BRIGHT}{increment} | {random_color}Total Tap: {Fore.WHITE + Style.BRIGHT}{total_increment}/3000 {Style.RESET_ALL}")
+        
+        time.sleep(2)
+
+def new_balance(cookie):
+    url = "https://www.kucoin.com/_api/xkucoin/platform-telebot/game/summary?lang=en_US"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Chromium\";v=\"111\", \"Not(A:Brand\";v=\"8\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-request-with": "null",
+        "Referer": "https://www.kucoin.com/miniapp/tap-game?inviterUserId=5496274031&rcode=QBSTAPN3",
+        "cookie": cookie
+    }
+    
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    balance = data.get("data").get("availableAmount")
+    print(f"{Fore.MAGENTA + Style.BRIGHT}New Balance: {balance}")
+
+def main():
+    file_path = "data.txt"
+    encoded_data_list = read_data_file(file_path)
+    
+    while True:
+        clear_terminal()
+        art()
+    
+        for index, encoded_data in enumerate(encoded_data_list, start=1):
+            print(f"{Fore.CYAN + Style.BRIGHT}------Account No.{index}------")
+            try:
+                decoded_data = decode_data(encoded_data)
+                cookie = login(decoded_data)
+                molecule = data(cookie)
+                tap(cookie, molecule)
+                new_balance(cookie)
+
+            except Exception as e:
+                print(f"{Fore.RED + Style.BRIGHT}Error: {str(e)}")
+                print(f"{Fore.YELLOW + Style.BRIGHT}Retrying in 5 seconds...")
+                countdown_timer(5)
+
+if __name__ == "__main__":
+    main()
